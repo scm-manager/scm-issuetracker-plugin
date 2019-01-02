bafc0cf4f347adeb937f68f060964f4e74a8c7b3
Philipp Czora <philipp.czora@cloudogu.com>
2019-01-02 14:22:27 +0100

    Reimplemented UI part

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -5,7 +5,14 @@ import { binder } from "@scm-manager/ui-extensions";
 import type { Changeset } from "@scm-manager/ui-types";
 import { escape } from "lodash";
 
+
+type Issue = {
+  name: string,
+  href: string
+}
+
 const ChangesetDescription = ({ changeset, description }) => {
+
   return (
     <p>
       {description.message.split("\n").map((item, key) => {
@@ -20,10 +27,9 @@ const renderSpan = (changeset: Changeset, key: string, item: string) => {
     return (
       <span
         key={key}
-        dangerouslySetInnerHTML={{
-          __html: replaceKeysWithLinks(item, changeset._links.issues) + "<br />"
-        }}
-      />
+      >
+        {React.createElement(React.Fragment, {}, ...replaceKeysWithLinks(item, changeset._links.issues))}
+      </span>
     );
   }
 
@@ -35,17 +41,38 @@ const renderSpan = (changeset: Changeset, key: string, item: string) => {
   );
 };
 
-const replaceKeysWithLinks = (line: string, issues) => {
-  let replacedString = escape(line);
-  for (let issue of issues) {
-    const link = createLink(issue.name, issue.href);
-    replacedString = replacedString.replace(issue.name, link);
+export const replaceKeysWithLinks = (line: string, issues: Issue[]) => {
+  let resultArray = [];
+  const issueMap = createIssueMap(issues);
+
+
+  const parts = line.split(" ");
+  for (let i = 0; i < parts.length; i++) {
+    let part = parts[i];
+
+    const issue = issueMap[part];
+    if (issue) {
+      resultArray.push(createLink(issue))
+    } else {
+      resultArray.push(part)
+    }
+    if (i < parts.length - 1) {
+      resultArray.push(" ")
+    }
   }
-  return replacedString;
+    return resultArray;
 };
 
-const createLink = (name: string, href: string) => {
-  return `<a href=${href}>${name}</a>`;
+const createIssueMap = (issues: Issue[]) => {
+  const issueMap = new Map();
+  for (const issue of issues) {
+    issueMap[issue.name] = issue
+  }
+  return issueMap;
+};
+
+export const createLink = (issue: Issue) => {
+  return <a href={issue.href}>{issue.name}</a>;
 };
 
 binder.bind("changesets.changeset.description", ChangesetDescription);
